93c085f1e5414c7e8508516b380fa23c51807aa4
PabloAirez <pabloadscontato@gmail.com>
2024-01-19 15:44:52 -0300

    Iniciando o projeto com react

---
+++ hsjeronimo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className='alert alert-success'>Sucesso</div>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
