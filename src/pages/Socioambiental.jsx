566f5302571829399f97fee341c62267feb3fd63
Pablo Aires dos Santos <pablo.santos@hsjeronimo.com.br>
2024-03-19 10:44:59 -0300

    Tirando os dois pontos do socioambiental

--- src/pages/Socioambiental.jsx
+++ src/pages/Socioambiental.jsx
@@ -12,11 +12,11 @@ const Socioambiental = () => {
     <>
     <Banner title={"Política Socioambiental"} backgroundImage={backgroundImage}></Banner>
     <Paragraph classNameText={"ml-20 mb-16 sm:mr-40 mr-10"} classNameTitle={" mt-10 ml-20 mb-10 sm:mr-40 mr-10"} title={"Sustentabilidade como prioridade"} text={"O Hospital Regional de São Jerônimo tem, em sua estratégia, a sustentabilidade ambiental como um dos mais importantes pilares de sustentação, pois entendemos que devemos preservar os recursos naturais cada vez mais escassos e minimizar os impactos ambientais decorrentes de nossas atividades."}></Paragraph>
-    <Accordion title={"Atendimento legal:"} text={"Cumprir com os requisitos legais e outros requisitos subscritos pela instituição;"}></Accordion>
-    <Accordion title={"Ética Socioambiental:"} text={"Criar canais de comunicação aberto junto ao público interno e externo, colaborando com o Desenvolvimento Sustentável;"}></Accordion>
-    <Accordion title={"Conscientização Ambiental:"} text={"Promover em todos os níveis a conscientização ambiental com foco no senso de responsabilidade individual e o sentido de prevenção com relação ao meio ambiente;"}></Accordion>
-    <Accordion title={"Controle dos Impactos Ambientais:"} text={"Promover alternativas para prevenir a poluição hídrica, da atmosfera e do solo;"}></Accordion>
-    <Accordion title={"Melhoria Contínua:"} text={"Prover recursos necessários à implantação desta política e de seu Sistema de Gestão Ambiental, melhorando continuamente suas atividades, processos e serviços."}></Accordion>
+    <Accordion title={"Atendimento legal"} text={"Cumprir com os requisitos legais e outros requisitos subscritos pela instituição;"}></Accordion>
+    <Accordion title={"Ética Socioambiental"} text={"Criar canais de comunicação aberto junto ao público interno e externo, colaborando com o Desenvolvimento Sustentável;"}></Accordion>
+    <Accordion title={"Conscientização Ambiental"} text={"Promover em todos os níveis a conscientização ambiental com foco no senso de responsabilidade individual e o sentido de prevenção com relação ao meio ambiente;"}></Accordion>
+    <Accordion title={"Controle dos Impactos Ambientais"} text={"Promover alternativas para prevenir a poluição hídrica, da atmosfera e do solo;"}></Accordion>
+    <Accordion title={"Melhoria Contínua"} text={"Prover recursos necessários à implantação desta política e de seu Sistema de Gestão Ambiental, melhorando continuamente suas atividades, processos e serviços."}></Accordion>
    
     </>
   )
